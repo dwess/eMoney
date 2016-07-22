f5f3d8930ffa2df257a83cc654fbeca22dfd5cec
dwess <dwess@udel.edu>
2016-07-21 20:57:24 -0400

    Initial Commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Web Browser Project.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.backButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.urlBar = new System.Windows.Forms.ToolStripTextBox();
+            this.goButton = new System.Windows.Forms.ToolStripButton();
+            this.browser = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backButton,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.urlBar,
+            this.goButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(735, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // backButton
+            // 
+            this.backButton.AccessibleName = "backButton";
+            this.backButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.backButton.Image = ((System.Drawing.Image)(resources.GetObject("backButton.Image")));
+            this.backButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(23, 22);
+            this.backButton.Text = "backButton";
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // urlBar
+            // 
+            this.urlBar.Name = "urlBar";
+            this.urlBar.Size = new System.Drawing.Size(200, 25);
+            this.urlBar.Click += new System.EventHandler(this.urlBar_Click);
+            // 
+            // goButton
+            // 
+            this.goButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.goButton.Image = ((System.Drawing.Image)(resources.GetObject("goButton.Image")));
+            this.goButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(23, 22);
+            this.goButton.Text = "toolStripButton4";
+            this.goButton.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // browser
+            // 
+            this.browser.Location = new System.Drawing.Point(33, 63);
+            this.browser.Multiline = true;
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(580, 399);
+            this.browser.TabIndex = 1;
+            this.browser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 517);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton backButton;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripTextBox urlBar;
+        private System.Windows.Forms.ToolStripButton goButton;
+        private System.Windows.Forms.TextBox browser;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Web Browser Project.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string url;
+        string html;
+        public Form1()
+        {
+            InitializeComponent();
+
+            html = "";
+            url = "ENTER URL HERE";
+            urlBar.Text = url;
+
+            browser.ReadOnly = true;
+            browser.TextAlign = HorizontalAlignment.Center;
+            browser.Text = "Hello World\r\nThis is your browser speaking";
+        }
+
+        private void urlBar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+           // webBrowser1.GoBack();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            //webBrowser1.GoForward();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            //webBrowser1.GoHome();
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            url = urlBar.Text;
+            browser.ResetText();
+            html = "";
+
+            ReadInFile();
+            FormatHtml();
+           
+            browser.Text = html;
+        }
+
+        private void FormatHtml()
+        {
+            //C: \Users\Denny\Documents\BitchinTestFolder/HelloWorld.html
+
+            //string[] splitHtml = html.Split(new char[] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries);
+            //Get Text between tags
+            List<string> splitHtml = new List<string>();
+            string currentWord = "";
+
+            foreach(char c in html)
+            {
+                currentWord += c;
+
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void ReadInFile()
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(url))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    html += line;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(exception.Message);
+                browser.Text = "404! Could not find File";
+            }
+        }
+    }
+}
